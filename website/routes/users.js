adb0360c13b60d1b1d269f2fa6b9b0903a9b29e3
prabhnith <prabhjot.nith@gmail.com>
2018-07-09 20:57:32 +0530

    NewsAPI added

--- website/routes/users.js
+++ website/routes/users.js
@@ -4,35 +4,26 @@ var router = express.Router();
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('2ecfb507a750494b965dcf6dce2ee3ce');
 
-var request = require('request');
-var cheerio = require('cheerio');
-
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 
-    // url = 'http://ezinearticles.com/?cat=Computers-and-Technology';
-    // url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=2ecfb507a750494b965dcf6dce2ee3ce';
     var articles = {};
     var travelguides = {};
+    var items = {};
 
-    newsapi.v2.sources({
-        category: 'technology',
-        language: 'en',
-        country: 'us'
-    }).then(response => {
+    newsapi.v2.topHeadlines().then(response => {
 
-        let items = response.articles;
-        console.log(items);
-        articles = items.slice(4, 12);
+        items = response.articles;
+        console.log(items.length);
+
         travelguides = items.slice(1, 4);
-        console.log("ARTICLE : ", articles);
+        articles = items.slice(5, 12);
+        // console.log("ARTICLE : ", travelguides);
+        res.render('index', {
+            articles: articles,
+            travelguides: travelguides
+        });
     });
-
-    res.render('index', {
-        articles: articles,
-        travelguides: travelguides
-    });
-
 });
 
 module.exports = router;
