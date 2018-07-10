f0a9b8315770df6597ce40d7fcf1b46a2bbbf135
prabhnith <prabhjot.nith@gmail.com>
2018-07-10 19:28:03 +0530

    partial views linked to json

--- website/routes/users.js
+++ website/routes/users.js
@@ -8,8 +8,11 @@ const newsapi = new NewsAPI('2ecfb507a750494b965dcf6dce2ee3ce');
 router.get('/', function (req, res, next) {
 
     var articles = {};
+    var featured = {};
     var travelguides = {};
+    var technology = {};
     var items = {};
+    var topics = {};
 
     newsapi.v2.topHeadlines().then(response => {
 
@@ -18,10 +21,18 @@ router.get('/', function (req, res, next) {
 
         travelguides = items.slice(1, 4);
         articles = items.slice(5, 12);
+        featured = items.slice(12, 16);
+        featured1 = items.slice(16, 20);
+        technology = items.slice(1, 9);
+        topics = items.slice(5, 8);
         // console.log("ARTICLE : ", travelguides);
         res.render('index', {
             articles: articles,
-            travelguides: travelguides
+            travelguides: travelguides,
+            featured: featured,
+            featured1: featured1,
+            technology:technology,
+            topics: topics
         });
     });
 });
