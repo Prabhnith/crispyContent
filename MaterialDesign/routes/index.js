197259a280340685a212c03059e6856b60e32afc
prabhnith <prabhjot.nith@gmail.com>
2018-08-01 16:31:46 +0530

    C:/Program Files/Git/postpage + .ejs added

--- MaterialDesign/routes/index.js
+++ MaterialDesign/routes/index.js
@@ -7,4 +7,9 @@ router.get('/', function (req, res, next) {
     res.render('homepage', { title: 'Express' });
 });
 
+router.get('/postpage', function (req, res, next) {
+    console.log("Postapage\n\n\n");
+    res.render('postpage', { title: 'Express' });
+});
+
 module.exports = router;
